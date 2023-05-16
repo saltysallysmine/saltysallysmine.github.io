e383322c7c85174bf37b77be2171b9041e8f3104
Valery Bergman <bergman.vd@yandex.ru>
2023-05-16 04:53:39 +0300

    Add arrows to gallery popup

--- js/gallery.js
+++ js/gallery.js
@@ -1,100 +1,108 @@
 let popupImageContainer = document.querySelector(".popup-image-container");
 let popupContainer = document.querySelector(".popup-container");
-let popupBackground = document.querySelector(".popup-background");
-popupOpened = false;
-currentIndex = null;
+let popupBackground = popupContainer.querySelector(".popup-background");
+let popupButtons = popupContainer.querySelector(".popup-buttons");
+let popupButtonPrev = popupButtons.querySelector(".lightbox-button-prev");
+let popupButtonNext = popupButtons.querySelector(".lightbox-button-next");
+
+let popupOpened = false;
+let currentIndex = null;
 
 let imgItems = document.querySelectorAll(".gallery-item");
 let imgHrefs = [];
 imgItems.forEach(link => { imgHrefs.push(link.href); });
 
 function showOpeningAnimation(linkToFullsizeImage, boundingRect) {
-    popupContainer.classList.add("popup-container--opened");
-    popupImageContainer.innerHTML = `
-        <img class="popup-image popup-image--opening" src=${linkToFullsizeImage} alt="Popup image" style="
-            position: absolute;
-            top: ${boundingRect.top}px;
-            left: ${boundingRect.left}px;
-            width: ${boundingRect.width}px;
-            height: ${boundingRect.height}px;
-        ">`;
+  popupContainer.classList.add("popup-container--opened");
+  popupImageContainer.innerHTML = `
+    <img class="popup-image popup-image--opening" src=${linkToFullsizeImage} alt="Popup image" style="
+  position: absolute;
+  top: ${boundingRect.top}px;
+  left: ${boundingRect.left}px;
+  width: ${boundingRect.width}px;
+  height: ${boundingRect.height}px;
+  ">`;
 }
 
 function openPopup(linkToFullsizeImage) {
-    popupOpened = true;
-    popupImageContainer.innerHTML = `<img class="popup-image" src=${linkToFullsizeImage} alt="full image">`
-    currentIndex = imgHrefs.findIndex(curLink => curLink == linkToFullsizeImage);
-    popupContainer.classList.add("popup-container--opened");
-    popupBackground.classList.add("popup-background--opened");
+  popupOpened = true;
+  popupImageContainer.innerHTML = `<img class="popup-image" src=${linkToFullsizeImage} alt="full image">`
+  currentIndex = imgHrefs.findIndex(curLink => curLink == linkToFullsizeImage);
+  popupContainer.classList.add("popup-container--opened");
+  popupBackground.classList.add("popup-background--opened");
+  popupButtonPrev.innerHTML = `<img class="lightbox-button-prev-image--opened") src="/src/img/png/arrow-left.png" alt="Left arrow">`
+  popupButtonNext.innerHTML = `<img class="lightbox-button-next-image--opened") src="/src/img/png/arrow-right.png" alt="Right arrow">`
 }
 
 function closePopup() {
-    popupOpened = false;
-    popupImageContainer.innerHTML = "";
-    currentIndex = null;
-    popupContainer.classList.remove("popup-container--opened");
-    popupBackground.classList.remove("popup-background--opened");
+  popupOpened = false;
+  popupImageContainer.innerHTML = "";
+  currentIndex = null;
+  popupContainer.classList.remove("popup-container--opened");
+  popupBackground.classList.remove("popup-background--opened");
+  popupButtonPrev.innerHTML = "";
+  popupButtonNext.innerHTML = "";
 }
 
 function switchPopupImage(step) {
-    if (currentIndex == null) {
-        return;
-    }
-    imgHrefsLength = imgHrefs.length;
-    currentIndex = currentIndex + step;
-    if (currentIndex < 0) {
-        currentIndex += imgHrefsLength;
-    }
-    if (currentIndex >= imgHrefsLength) {
-        currentIndex -= imgHrefsLength;
-    }
-    currentIndex = currentIndex;
-    let currentLink = imgHrefs[currentIndex];
-    openPopup(currentLink);
+  if (currentIndex == null) {
+    return;
+  }
+  imgHrefsLength = imgHrefs.length;
+  currentIndex = currentIndex + step;
+  if (currentIndex < 0) {
+    currentIndex += imgHrefsLength;
+  }
+  if (currentIndex >= imgHrefsLength) {
+    currentIndex -= imgHrefsLength;
+  }
+  currentIndex = currentIndex;
+  let currentLink = imgHrefs[currentIndex];
+  openPopup(currentLink);
 }
 
 function nextPopupImage() {
-    switchPopupImage(1);
+  switchPopupImage(1);
 }
 
 function previousPopupImage() {
-    switchPopupImage(-1);
+  switchPopupImage(-1);
 }
 
 imgItems.forEach(
-    link => {
-        link.addEventListener('click', async function (event) {
-            event.preventDefault();
-            let image = link.querySelector('img');
-            let linkToFullsizeImage = event.currentTarget.href;
-            let boundingRect = image.getBoundingClientRect();
-            showOpeningAnimation(linkToFullsizeImage, boundingRect);
-            await new Promise(_ => setTimeout(_, 1000));
-            openPopup(linkToFullsizeImage);
-        })
-    }
+  link => {
+    link.addEventListener('click', async function (event) {
+      event.preventDefault();
+      let image = link.querySelector('img');
+      let linkToFullsizeImage = event.currentTarget.href;
+      let boundingRect = image.getBoundingClientRect();
+      showOpeningAnimation(linkToFullsizeImage, boundingRect);
+      await new Promise(_ => setTimeout(_, 1000));
+      openPopup(linkToFullsizeImage);
+    })
+  }
 );
 
 document.addEventListener('keydown', event => {
-    let keyName = event.key;
-    if (popupOpened) {
-        switch (keyName) {
-            case "Escape":
-                closePopup();
-                break;
-            case "ArrowRight":
-            case "k":
-                nextPopupImage();
-                break;
-            case "ArrowLeft":
-            case "j":
-                previousPopupImage();
-                break
-        }
+  let keyName = event.key;
+  if (popupOpened) {
+    switch (keyName) {
+      case "Escape":
+        closePopup();
+        break;
+      case "ArrowRight":
+      case "l":
+        nextPopupImage();
+        break;
+      case "ArrowLeft":
+      case "h":
+        previousPopupImage();
+        break
     }
-    // console.log(keyName);
+  }
+  // console.log(keyName);
 });
 
 popupBackground.addEventListener('click', event => {
-    closePopup();
+  closePopup();
 });
